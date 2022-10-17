c516e83824532d9fabe3f1eda467d1760b49b7d5
grant h <huiras.grant@gmail.com>
2022-10-17 16:50:02 -0500

    Update user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,9 @@
 const { User, Thought } = require('../models');
 const userControl = {
+    getAllUser({ params }, res) {
+        User.find({ })
+        .then((dbUserdata) => res.json(dbUserdata))
+        .catch((err) => res.status(500).json(err))
+    },
 
 }
