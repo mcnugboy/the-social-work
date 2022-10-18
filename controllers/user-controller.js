7a0dc7df5fc34b2f04a21d7304bf84978390d0fd
grant h <huiras.grant@gmail.com>
2022-10-18 09:00:41 -0500

    Added controllers for finding a user and creating a new user.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,12 +1,31 @@
 const { User, Thought } = require('../models');
 const userControl = {
-    getAllUser({ params }, res) {
+    getAllUser({params}, res) {
         User.find({ })
         .then((dbUserdata) => res.json(dbUserdata))
         .catch((err) => res.status(500).json(err))
     },
-    getUserById({ params }, res) {
-        User.findOne({ _id: params.id })
-        
-    }
+    getUserById({params}, res) {
+        User.findOne({_id: params.id})
+        .populate('thoughts')
+        .populate('friends')
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserdata) {
+                res.status(400).json({message: 'No users found with this id.'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    createUser({body}, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err))
+    },
+    
 }
